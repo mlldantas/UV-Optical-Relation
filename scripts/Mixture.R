be2de8ac5fae5111fefe68d5e2b2bc92b42af824
Rafael S. de Souza <rafael.2706@gmail.com>
2016-03-07 14:33:19 +0100

    mixture

--- scripts/Mixture.R
+++ scripts/Mixture.R
@@ -4,27 +4,41 @@ data<-read.csv("https://raw.githubusercontent.com/mdastro/UV_Optical/master/Data
 
 
 g.r<-data$g.r
-FUV<-data$FUV.NUV
+FUV.NUV<-data$FUV.NUV
 
 
-# Run mixture model
+# Run mixture model g-r
 mixmdl = normalmixEM(g.r)
 pdf("..//Figures/gr.pdf",height = 8,width = 8)
 plot(mixmdl,which=2,xlab2="g-r",ylab2="Densidade")
 lines(density(g.r), lty=2, lwd=2)
 dev.off()
 
-mixmdl3 = normalmixEM(FUV)
-pdf("FUV.NUV.pdf",height = 8,width = 8)
-plot(mixmdl3,which=2,xlab2="FUV-NUV",ylab2="Densidade")
-lines(density(FUV), lty=2, lwd=2)
+
+
+# Run mixture model FUV-NUV
+mixmdl2 = normalmixEM(FUV.NUV)
+pdf("..//Figures/FUV.NUV.pdf",height = 8,width = 8)
+plot(mixmdl2,which=2,xlab2="FUV-NUV",ylab2="Densidade")
+lines(density(FUV.NUV), lty=2, lwd=2)
 dev.off()
 
+
+
+# Run mixture model g-r vs FUV-NUV
 data2<-data.frame(data$g.r,data$FUV.NUV)
-mixmdl2 = mvnormalmixEM(data2)
-pdf("bivariate.pdf",height = 8,width = 8)
-plot(mixmdl2,which=2,ylab2="FUV-NUV",xlab2="g-r")
+mixmdl3 = mvnormalmixEM(data2)
+pdf("..//Figures/bivariate.pdf",height = 8,width = 8)
+plot(mixmdl3,which=2,ylab2="FUV-NUV",xlab2="g-r")
 dev.off()
+
+
+# Stops here
+
+
+
+
+
 
 
 colnames(data2)<-c("g-r","FUV-NUV")
@@ -49,5 +63,4 @@ gdata$class<-as.factor(gdata$class)
 ggplot(gdata,aes(x=FUV,group=class))+
   geom_histogram(binwidth=0.1,aes(fill=class,alpha=0.2))
 
-ggplot(gdata,aes(x=FUV))+
-  geom_density()
+ggplot(gdata,aes(x=FUV))+geom_density()
