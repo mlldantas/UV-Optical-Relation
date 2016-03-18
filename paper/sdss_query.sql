b066cf52cea5a7d0b094e6f5911feae8b8224a83
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2016-03-18 18:53:52 -0300

    Update sdss_query.sql

--- paper/sdss_query.sql
+++ paper/sdss_query.sql
@@ -7,7 +7,7 @@ SELECT res.Objid, res.plate, res.mjd, res.fiberid, res.ra, res.dec, glx.fiberMag
 INTO MyDB.MyData_GALEX_SDSS
 FROM MyDB.MyData_GALEX AS res, Galaxy AS glx, SpecObjAll AS s
 WHERE glx.nchild = 0 AND (glx.Flags & (0x0000000000040000 + 0x0000000000000002 + 0x0000080000000000)) = 0 
-  AND (glx.dered_u BETWEEN 16 AND 25) AND (glx.dered_g BETWEEN 16 AND 25) AND (glx.dered_r BETWEEN 16 AND 22) AND (glx.dered_i BETWEEN 16 AND 25 
+  AND (glx.dered_u BETWEEN 16 AND 25) AND (glx.dered_g BETWEEN 16 AND 25) AND (glx.dered_r BETWEEN 16 AND 22) AND (glx.dered_i BETWEEN 16 AND 25) 
     AND (glx.dered_z BETWEEN 16 AND 25) AND (s.specClass = 2) AND (s.zStatus > 1) AND (s.zConf > 0.9) AND (s.zWarning = 0) AND (res.Objid = glx.objid) 
-    AND (glx.objid = s.bestObjID) AND (s.z BETWEEN 0.05 AND 0.075) AND (glx.petroR90_r BETWEEN 1.5 AND 3.5))
+    AND (glx.objid = s.bestObjID) AND (s.z BETWEEN 0.05 AND 0.075) AND (glx.petroR90_r BETWEEN 1.5 AND 3.5)
 ORDER BY res.plate, res.mjd, res.fiberid
