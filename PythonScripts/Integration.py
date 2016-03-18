ebb98e261578052465c66d9411ea960a1ea3246a
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2016-03-18 19:54:35 -0300

    Update and rename Integration.py to PythonScripts/Integration.py
    Working program. Changes made from older version. Needs: save output data data as csv file.

--- PythonScripts/Integration.py
+++ PythonScripts/Integration.py
@@ -8,14 +8,13 @@
     @version: 0.1.9
     This program uses synthetic spectra produced by STARLIGHT and adjusts observational data using chi^2 minimization
     method.
-    
-    This version takes into account the fiber correction in the flux calculation, and also takes into account SDSS' dereddenned 
+
+    This version takes into account the fiber correction in the flux calculation, and also takes into account SDSS'
+    deredenned
     magnitudes and Fitzpatrick's Law to correct the UV magnitudes, and filter responses.
-    
+
     In this version we completed the loop in order to analyze all the data and implement a single output.
     For more information about the input data, please read the schema text file.
-    
-    This is an incomplete version with several modifications applied!!!
 """
 
 from __future__ import division
@@ -34,13 +33,13 @@ from cardelli import cardelli_redlaw
 if __name__ == '__main__':
 
     # Configuring the inputs -------------------------------------------------------------------------------------------
-    
-    # results_path      = '/home/mldantas/Dropbox/MSc_paper/Programs/Results'
-    
-    spec_files_path   = 'Spectra'
-    data_obs          = 'MyData_GALEX_SDSS_clean.csv'
-    filelist          = 'filelistnorm.txt'
-    pivot_wavelengths = 'pivot_lambdas.txt'
+
+    results_path      = '/home/mldantas/Dropbox/MSc_paper/Programs/Fig'
+    spec_files_path   = '/home/mldantas/Dropbox/MSc_paper/Programs/EspectrosRebinadosSelfZ'
+    data_obs          = '/home/mldantas/Dropbox/MSc_paper/Dataset/MyData_GALEX_SDSS_clean.csv'
+    #data_obs          = '/home/mldantas/Dropbox/MSc_paper/Dataset/new_teste.txt'
+    filelist          = '/home/mldantas/Dropbox/MSc_paper/Programs/filelistnorm.txt'
+    pivot_wavelengths = '/home/mldantas/Dropbox/MSc_paper/Programs/pivot_lambdas.txt'
 
     # Constants --------------------------------------------------------------------------------------------------------
     c = 2.99792458E18	# Light Speed in Angstrom/s
@@ -55,25 +54,34 @@ if __name__ == '__main__':
     for i in range(len(my_data[0, :])):                             # Converting numpy array into dictionary
         dictionary[my_data[0, i]] = np.array(my_data[0 + 1:, i], dtype=str)
 
-    object_id      = dictionary['Objid'].astype(long)
-    plate          = dictionary['plate'].astype(int)
-    mjd            = dictionary['mjd'].astype(int)
-    fiberid        = dictionary['fiberid'].astype(int)
-    fiber_mag_z    = dictionary['fiberMag_z'].astype(float)
-    model_mag_u    = dictionary['modelMag_u'].astype(float)
-    model_mag_g    = dictionary['modelMag_g'].astype(float)
-    model_mag_r    = dictionary['modelMag_r'].astype(float)
-    model_mag_i    = dictionary['modelMag_i'].astype(float)
-    model_mag_z    = dictionary['modelMag_z'].astype(float)
-    mag_fuv        = dictionary['fuv_mag'].astype(float)
-    mag_nuv        = dictionary['nuv_mag'].astype(float)
-    extinction_u   = dictionary['dered_u'].astype(float)
-    extinction_g   = dictionary['dered_g'].astype(float)
-    extinction_r   = dictionary['dered_r'].astype(float)
-    extinction_i   = dictionary['dered_i'].astype(float)
-    extinction_z   = dictionary['dered_z'].astype(float)
-    mag_dered_sdss = my_data[1:, 14:18].astype(float) #TODO check if it is reading everything alright, 5 rows 
-    e_bv           = dictionary['e_bv'].astype(float)
+    object_id       = dictionary['Objid'].astype(long)
+    plate           = dictionary['plate'].astype(int)
+    mjd             = dictionary['mjd'].astype(int)
+    fiberid         = dictionary['fiberid'].astype(int)
+    fiber_mag_z     = dictionary['fiberMag_z'].astype(float)
+    model_mag_u     = dictionary['modelMag_u'].astype(float)
+    model_mag_g     = dictionary['modelMag_g'].astype(float)
+    model_mag_r     = dictionary['modelMag_r'].astype(float)
+    model_mag_i     = dictionary['modelMag_i'].astype(float)
+    model_mag_z     = dictionary['modelMag_z'].astype(float)
+    mag_fuv         = dictionary['fuv_mag'].astype(float)
+    mag_nuv         = dictionary['nuv_mag'].astype(float)
+    extinction_u    = dictionary['extinction_u'].astype(float)
+    extinction_g    = dictionary['extinction_g'].astype(float)
+    extinction_r    = dictionary['extinction_r'].astype(float)
+    extinction_i    = dictionary['extinction_i'].astype(float)
+    extinction_z    = dictionary['extinction_z'].astype(float)
+    mag_dered_sdss  = my_data[1:, 14:19].astype(float)
+    e_bv            = dictionary['e_bv'].astype(float)
+    redshift        = dictionary['redshift'].astype(float)
+    model_mag_u_err = dictionary['modelMagErr_u'].astype(float)
+    model_mag_g_err = dictionary['modelMagErr_g'].astype(float)
+    model_mag_r_err = dictionary['modelMagErr_r'].astype(float)
+    model_mag_i_err = dictionary['modelMagErr_i'].astype(float)
+    model_mag_z_err = dictionary['modelMagErr_z'].astype(float)
+    mag_fuv_err     = dictionary['fuv_magerr'].astype(float)
+    mag_nuv_err     = dictionary['nuv_magerr'].astype(float)
+
 
     # Loading filelist -------------------------------------------------------------------------------------------------
     filelist = np.loadtxt(filelist, dtype=str)
@@ -86,8 +94,10 @@ if __name__ == '__main__':
     mag_ab_i = model_mag_i - extinction_i + 0.01
     mag_ab_z = model_mag_z - extinction_z + 0.02
 
-    mag_ab_sdss      = np.array([mag_ab_u, mag_ab_g, mag_ab_r, mag_ab_i, mag_ab_z]).T
-    mag_ab_sdss_griz = np.array([mag_ab_g, mag_ab_r, mag_ab_i, mag_ab_z]).T
+    mag_ab_sdss        = np.array([mag_ab_u, mag_ab_g, mag_ab_r, mag_ab_i, mag_ab_z]).T
+    mag_ab_sdss_griz   = np.array([mag_ab_g, mag_ab_r, mag_ab_i, mag_ab_z]).T
+    model_mag_err_sdss = np.array([model_mag_u_err, model_mag_g_err, model_mag_r_err, model_mag_i_err,
+                                   model_mag_z_err]).T
 
 
     # Matching observational data with synthetic data ------------------------------------------------------------------
@@ -131,7 +141,7 @@ if __name__ == '__main__':
                                                                                 # AB mag into flux + 1e-17 normalization
         # Calculating g,r,i,z SDSS fluxes ------------------------------------------------------------------------------
         ## The following fluxes already account for extinction and K correction ----------------------------------------
-        flux_sdss_griz = ((c/(wavelengths_sdss_griz ** 2.)) * 10.**(0.4 * (fiber_mag_z[index]-model_mag_u[index])) * 
+        flux_sdss_griz = ((c/(wavelengths_sdss_griz ** 2.)) * 10.**(0.4 * (fiber_mag_z[index]-model_mag_u[index])) *
                      10. ** (-0.4 * (mag_ab_sdss_griz[index][0] + 48.60))/1E-17)
                                                                                 # AB mag into flux + 1e-17 normalization
 
@@ -148,6 +158,8 @@ if __name__ == '__main__':
         flux_fuv_fitzpatrick = fm_unred(fuv_obs_wavelength, flux_fuv, e_bv[index])    # FUV Flux corrected by extinction
         flux_fuv_fitzpatrick = flux_fuv_fitzpatrick / (1E-17)                                      # 1e-17 normalization
 
+        flux_galex = np.array([flux_fuv_fitzpatrick, flux_nuv_fitzpatrick])
+
         # Reading the synthetic spectra files --------------------------------------------------------------------------
         synthetic_wavelengths = np.loadtxt(os.path.join(spec_files_path, spec_file), usecols=[0])
         synthetic_fluxes      = np.loadtxt(os.path.join(spec_files_path, spec_file), usecols=[1])
@@ -161,21 +173,40 @@ if __name__ == '__main__':
         denominator_sum = 0
         for p in range(wavelengths_sdss_griz.size):
             index2 = np.abs(wavelengths_sdss_griz[p] - synthetic_wavelengths).argmin()
-            numerator_sum   = numerator_sum + (flux_sdss[p] * synthetic_fluxes[index2])
-            denominator_sum = denominator_sum + (flux_sdss[p] ** 2.)
+            numerator_sum   = numerator_sum + (flux_sdss_griz[p] * synthetic_fluxes[index2])
+            denominator_sum = denominator_sum + (flux_sdss_griz[p] ** 2.)
         correction_factor = (numerator_sum / denominator_sum)
 
-        synthetic_fluxes_corr = synthetic_fluxes * correction_factor
+        flux_sdss_corr  = flux_sdss * correction_factor
+        flux_nuv_corr   = flux_nuv_fitzpatrick * correction_factor
+        flux_fuv_corr   = flux_fuv_fitzpatrick * correction_factor
+        flux_galex_corr = np.array([float(flux_fuv_corr), float(flux_nuv_corr)])
+        flux_all_corr   = np.array([float(flux_sdss_corr[0]), float(flux_sdss_corr[1]), float(flux_sdss_corr[2]),
+                                  float(flux_sdss_corr[3]), float(flux_sdss_corr[4]), float(flux_fuv_corr),
+                                  float(flux_nuv_corr)])
+
+        # Chi^2 selection  ---------------------------------------------------------------------------------------------
+        flux_sdss_synth = []
+        for n in range(wavelengths_sdss_griz.size):
+            index_flux_synth = np.abs(wavelengths_sdss_griz[n] - synthetic_wavelengths).argmin()
+            flux_synth = synthetic_fluxes[index_flux_synth]
+            flux_sdss_synth.append(flux_synth)
+        flux_sdss_synth = np.array(flux_sdss_synth)
+
+        goodness = ['goodness']
+        goodness = s.chisquare(flux_sdss_corr[1:], flux_sdss_synth)
+        absolute_goodness = goodness[0]/3
+
 
         # UV Synthetic equivalent fluxes -------------------------------------------------------------------------------
         k = np.abs(wavelengths_galex[0] - synthetic_wavelengths).argmin()
-        fuv_synth_flux = synthetic_fluxes_corr[k]
+        fuv_synth_flux = synthetic_fluxes[k]
 
         m = np.abs(wavelengths_galex[1] - synthetic_wavelengths).argmin()
-        nuv_synth_flux = synthetic_fluxes_corr[m]
+        nuv_synth_flux = synthetic_fluxes[m]
 
         # Absolute Magnitudes using Luminosity Distance ----------------------------------------------------------------
-        luminosity_distance = cosmo.cosmocalc(z[index])['DL_Mpc']
+        luminosity_distance = cosmo.cosmocalc(redshift[index])['DL_Mpc']
         mag_fuv_abs         = mag_fuv_dered_fm - 5 * np.log10(luminosity_distance) - 25
         mag_nuv_abs         = mag_nuv_dered_fm - 5 * np.log10(luminosity_distance) - 25
         mag_u_abs           = mag_ab_u[index] - 5 * np.log10(luminosity_distance) - 25
@@ -185,12 +216,12 @@ if __name__ == '__main__':
         mag_z_abs           = mag_ab_z[index] - 5 * np.log10(luminosity_distance) - 25
 
         # SDSS Fluxes Uncertainty --------------------------------------------------------------------------------------
-        mmag_sdss_err_index = mmag_sdss_err[index][0]
-        sigma_flux = np.zeros(5, dtype = np.dtype([('error_flux_u', '<f4'), ('error_flux_g', '<f4'), ('error_flux_r', '<f4'), ('error_flux_i', '<f4'), ('error_flux_z', '<f4'))]))
+        model_mag_err_sdss_index = model_mag_err_sdss[index][0]
+        sigma_flux=[]
         for i in range(mag_dered_sdss[index].size):
-            sigma_magnitude_square = mmag_sdss_err_index[i] ** 2
+            sigma_magnitude_square = model_mag_err_sdss_index[i] ** 2
             sigma_flux_i = \
-                np.sqrt(((((c/(wavelengths_sdss[i]**2)) * 1E17) * (0.4**2) * (mag_ab_sdss[i][0] + 48.60) *
+                np.sqrt(((((c/(wavelengths_sdss[i]**2))) * (0.4**2) * (mag_ab_sdss[i][0] + 48.60) *
                           10 ** (-0.4 * (mag_ab_sdss[i][0] + 48.60) - 1.)) ** 2) * sigma_magnitude_square) \
                 * wavelengths_sdss[i]
             sigma_flux.append(float(sigma_flux_i))
@@ -200,36 +231,21 @@ if __name__ == '__main__':
         ## FUV ---------------------------------------------------------------------------------------------------------
         sigma_fuv_flux = ['error_flux_fuv']
         sigma_fuv_flux = ((1.4 * 16 * (mag_fuv_dered_fm - 18.82) *
-                          (10 ** (-0.4 * (mag_fuv_dered_fm - 18.82) - 1.))) * mag_fuv_err[index]) \
-                        * fuv_obs_wavelength
+                          (10 ** (-0.4 * (mag_fuv_dered_fm - 18.82) - 1.))) * mag_fuv_err[index]) * fuv_obs_wavelength
 
 
         ## NUV ---------------------------------------------------------------------------------------------------------
         sigma_nuv_flux = ['error_flux_nuv']
         sigma_nuv_flux = ((20.6 * (0.4**2) * (mag_nuv_dered_fm - 20.08) *
-                          (10 ** (-0.4 * (mag_nuv_dered_fm - 20.08) - 1.))) * mag_nuv_err[index]) \
-                         * nuv_obs_wavelength
+                          (10 ** (-0.4 * (mag_nuv_dered_fm - 20.08) - 1.))) * mag_nuv_err[index]) * nuv_obs_wavelength
 
 
         sigma_uv = np.array([float(sigma_fuv_flux), float(sigma_nuv_flux)])
 
-        #Saving the results in a new file ----------------------------------------------------------------------------
-        input_file  = open('MyData_GALEX_SDSS_clean.csv')
-        reader_file = csv.reader(input_file)
-        with open('output.csv', 'w') as file_out:
-        with input_file as in_file:
-            i = 0
-            for line in in_file:
-                s = ''
-                s = line.replace('\n', '') + ',' + column[i] + '\n'
-                i+=1
-                file_out.write(s)
-
-
 
         # Plotting the results -----------------------------------------------------------------------------------------
         plt.semilogy(synthetic_wavelengths, synthetic_fluxes * synthetic_wavelengths, '-', linewidth=1., color='black',
-                  label = r" Starlight Spectrum Object {0:s}".format(basename))
+                     alpha=1., label = r" Starlight Spectrum Object {0:s}".format(basename))
         plt.semilogy(wavelengths_sdss, flux_sdss_corr * wavelengths_sdss, 'o', markersize=15., color="#FF4500",
                      alpha=0.5, label = 'SDSS fluxes')
         plt.errorbar(wavelengths_sdss, flux_sdss_corr * wavelengths_sdss, yerr=sigma_flux, fmt='.', color='#FF4500',
@@ -255,12 +271,11 @@ if __name__ == '__main__':
         #           color='red', fc='red', ec='red', alpha=0.6)
         # plt.tick_params('both', labelsize='28')
         plt.savefig(os.path.join(results_path, basename+'_final.png'), dpi = 100)
-        #plt.show()
+        # plt.show()
         plt.clf()
-        #exit()
+        # exit()
 
-
-    parameters_out.close()
+    #parameters_out.close()
 
 
 __author__ = 'mldantas'
